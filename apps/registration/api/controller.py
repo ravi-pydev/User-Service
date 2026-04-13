610ec3081f4e060215f882c11594ddd866f7b6fd
RaviDarshan <ravi.darshan@careers360.com>
2026-04-13 09:12:26 +0530

    add controller

--- apps/registration/api/controller.py
+++ apps/registration/api/controller.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
 # Create your views here.
+# import redis
+from django.conf import settings
+from django.http import HttpResponse
+
+from rest_framework.views import APIView
+
+# from utils.helpers.decorators import log_api_response
+
+
+class Home(APIView):
+	""" 
+	CUS Home API
+	Permissions:
+		- ApiKeyPermission: Ensures requests include a valid API key.
+	"""
+
+	# @log_api_response
+	def get(self, request):
+		return HttpResponse("API Backend for Common User Service")
+
--- defaulf_config/urls.py
+++ defaulf_config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.registration.urls import user_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', admin.site.urls),
 ]
+urlpatterns = urlpatterns+user_urlpatterns
