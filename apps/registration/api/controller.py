f0fe303da7e49af1c756da1390b2f68857bf4e0d
RaviDarshan <ravi.darshan@careers360.com>
2026-04-15 16:21:47 +0530

    Add hello world API endpoint at /api/hello/

--- apps/registration/api/controller.py
+++ apps/registration/api/controller.py
@@ -1,5 +1,15 @@
 from django.shortcuts import redirect, render
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+class HelloWorldView(APIView):
+    """
+    Returns a simple Hello World JSON response.
+    """
+
+    def get(self, request):
+        return Response({"message": "Hello, World!"})
 
 
 class Home(APIView):
